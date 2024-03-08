a235ffe42d583c13851017bfd6ed336e8c357ba3
Mukund Mahajan <mukund@Mukunds-MacBook-Pro.local>
2024-03-08 21:58:44 +0530

    Added `es-lint` support

--- src/components/TimeQuote.jsx
+++ src/components/TimeQuote.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import './styles/timer.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./styles/timer.css";
 
 const TimeQuote = () => {
-    const [dateState, setDateState] = useState(new Date());
-    useEffect(() => {setInterval(() => setDateState(new Date()), 30000);}, []);
-    return (
-      <div className="quote">
-        <div className='timer'>
-        <p className='date'>
-          {' '}
-          {dateState.toLocaleDateString('en-GB', {
-            day: 'numeric',
-            month: 'short',
-            year: 'numeric',
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+  return (
+    <div className="quote">
+      <div className="timer">
+        <p className="date">
+          {" "}
+          {dateState.toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
           })}
         </p>
-        <p class='time'>
-        { dateState.toLocaleString('en-US', {
-          hour: 'numeric',
-          minute: 'numeric',
-          hour12: true,
-        })}
+        <p className="time">
+          {dateState.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          })}
         </p>
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default TimeQuote
+export default TimeQuote;
--- src/data/channels.js
+++ src/data/channels.js
@@ -10,17 +10,16 @@ import img9 from '../assets/imgs/img9.png'
 import img10 from '../assets/imgs/img10.png'
 
 const channels = [
+  { name: 'Lofi Boy', url: 'https://www.youtube.com/live/6D2p1UYM1Yw?feature=share', img: img2 },
+  { name: 'Lofi Girl Chill', url: 'https://www.youtube.com/live/zsYwejVYZ_M?feature=share', img: img3 },
+  { name: 'Study Aesthetic', url: 'https://www.youtube.com/live/XstikgbeTPI?feature=share', img: img4 },
+  { name: 'Pink Cloud', url: 'https://www.youtube.com/watch?v=wkhLHTmS_GI', img: img5 },
+  { name: 'Calm Evening', url: 'https://www.youtube.com/watch?v=dxUtV-zNv9w', img: img6 },
+  { name: 'Bootleg Boy', url: 'https://www.youtube.com/watch?v=Su00mfi5QUY', img: img7 },
+  { name: 'STEEZYASFUCK', url: 'https://www.youtube.com/watch?v=efwDBb84yWY', img: img8 },
+  { name: 'Purple Cat', url: 'https://www.youtube.com/watch?v=bJUO1WnjXQY', img: img9 },
+  { name: 'SpongeBob Lofi', url: 'https://www.youtube.com/watch?v=tfUPbO_vGzM', img: img10 },
+  { name: 'Lofi Geek', url: 'https://www.youtube.com/watch?v=tfBVp0Zi2iE', img: img1 }
+]
 
-  { name: 'Lofi Boy', url:'https://www.youtube.com/live/6D2p1UYM1Yw?feature=share',img: img2 },
-  { name: 'Lofi Girl Chill', url: 'https://www.youtube.com/live/zsYwejVYZ_M?feature=share',img: img3},
-  { name: 'Study Aesthetic', url: 'https://www.youtube.com/live/XstikgbeTPI?feature=share',img: img4},
-  { name: 'Pink Cloud', url: 'https://www.youtube.com/watch?v=wkhLHTmS_GI',img: img5},
-  { name: 'Calm Evening', url: 'https://www.youtube.com/watch?v=dxUtV-zNv9w',img: img6},
-  { name: 'Bootleg Boy', url: 'https://www.youtube.com/watch?v=Su00mfi5QUY',img: img7},
-  { name: 'STEEZYASFUCK', url: 'https://www.youtube.com/watch?v=efwDBb84yWY',img: img8},
-  { name: 'Purple Cat', url: 'https://www.youtube.com/watch?v=bJUO1WnjXQY',img: img9},
-  { name: 'SpongeBob Lofi', url: 'https://www.youtube.com/watch?v=tfUPbO_vGzM',img: img10},
-  { name: 'Lofi Geek', url: 'https://www.youtube.com/watch?v=tfBVp0Zi2iE', img: img1}
-];
-
-export default channels;
+export default channels
